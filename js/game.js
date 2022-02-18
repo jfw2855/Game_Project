c107b44e7a1cd9cb1d01c94e0b9d87603423086a
Justin Waddle <justinfrankwaddle@gmail.com>
2022-02-18 15:17:43 -0600

    Commit 2 - Added pseudo code to outline js needed for project

--- js/game.js
+++ js/game.js
@@ -1,1 +1,37 @@
 console.log("game.js connected to index")
+
+// Defined DOM variables
+
+// defined variables to keep track of the gameplay (playerScore, round)
+
+// class for decades which will have a constructor (decades,songs)
+    // song objects that will store the title, artist, lyrics [], questions [], answers [], choices []
+
+
+// Functions ~~~~~~~~~~~~~~~~
+
+// startGame - hide homescreen elements and move to next
+    // keeps track of rounds
+    // if rounds is > 3 , will move to endGame
+
+// playround - will animate the lyrics and then prompt player 1 what is the next line
+
+// checkRight - will determine if the player selected the right answer
+    // updates score based on right/wrong answer
+
+// recall startGame to move to next round
+    // will switch to the next song and remove the lyrics from previous song
+
+// endGame - removes all play game divs and shows endGame screen
+
+
+// Eventlisteners ~~~~~~~~~~~~~~~
+
+// Play button event listener
+
+// instructions hover
+
+// player selection from form dropdown 
+
+// homescreen button
+
